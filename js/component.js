88b656cc978ad3a8b2f70b7983bae4edace0c2fe
bugman001 <jhlee@bitnine.net>
2021-02-05 15:29:35 +0900

    스크롤바

--- js/component.js
+++ js/component.js
@@ -194,6 +194,15 @@ function menuActive() {
 
 menuActive();
 
+let vh = window.innerHeight * 0.01
+
+document.documentElement.style.setProperty('--vh', `${vh}px`)
+
+window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01
+    document.documentElement.style.setProperty('--vh', `${vh}px`)
+})
+
 $("#language").ddslick({
     width: "105px",
     imagePosition: "left",
